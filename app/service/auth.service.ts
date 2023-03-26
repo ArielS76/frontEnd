4605d6401ec660f1e45b97983903ef88f95ba73c
arielSolis <108560263+ArielS76@users.noreply.github.com>
2023-03-26 16:57:47 -0300

    Add files via upload

---
+++ app/service/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { JwtDto } from '../model/jwt-dto';
+import { LoginUsuario } from '../model/login-usuario';
+import { NuevoUsuario } from '../model/nuevo-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  //authURL = 'http://localhost:8080/';
+
+  URL = environment.URL + 'auth/';
+
+
+  constructor(private httpClient: HttpClient) { }
+
+ public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
+   return this.httpClient.post<any>(this.URL + 'nuevo', nuevoUsuario);
+ }
+
+ public login(loginUsuario: LoginUsuario): Observable<JwtDto>{
+   return this.httpClient.post<JwtDto>(this.URL + 'login', loginUsuario)
+ }
+}
